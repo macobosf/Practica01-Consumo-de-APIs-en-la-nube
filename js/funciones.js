fd154cce20d2c24cebd9349ef72680cfb91d90c2
Marco <marcocobos15@gmail.com>
2021-04-13 00:01:19 -0500

    Actulizacion-4

---
+++ js/funciones.js
@@ -0,0 +1,47 @@
+export  const $=qs=>document.querySelector(qs);
+
+export const mostrarAlerta=mensaje=>{
+    const alerta=$('.alerta');
+    alerta.innerHTML=`
+        <div class="alert alert-danger" role="alert">
+        <strong>${mensaje}</strong>
+        </div>
+    `;
+
+    //Limpia el mensaje de alerta cada 2 segundos
+    setTimeout(() =>{ 
+        alerta.innerHTML='';
+    }, 2000);
+}
+
+export const mostrarInfo=({sprites:{front_default},name,types})=>{
+    const info=$('.info');
+    const div=document.createElement('div');
+    div.classList.add('ml-3');
+    div.innerHTML=`
+    
+    <div class="pokemon w-25 my-2">
+    <img src="${front_default}" style="width: 100%;"></img>
+    </div>
+    <h6 class = ¨text-warning¨>Nombre:</h6>
+    <h5 class = ¨text-white text-uppercase¨>${name}</h5>
+    <h6 class = ¨text-warning¨>Tipo:</h6>
+    
+    `;
+    
+    types.forEach(({type:{name}}) => {
+        let ul = document.createElement('ul');
+        ul.innerHTML=`
+        
+        <li class = "text-info">
+                    <h5 class="text-white text-uppercase"> 
+                        ${name}
+                    </h5>
+                </li>
+        
+        `;
+        div.appendChild(ul);
+    });
+    info.appendChild(div);
+    
+}
